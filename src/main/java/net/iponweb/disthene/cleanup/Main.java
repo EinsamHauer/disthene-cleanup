2f141572103b82858ed2597a2f4ba6032a0a7bb6
Andrei Ivanov <Andrei.B.Ivanov@gmail.com>
2015-10-22 12:31:32 +0300

    initial commit

---
+++ src/main/java/net/iponweb/disthene/cleanup/Main.java
@@ -0,0 +1,13 @@
+package net.iponweb.disthene.cleanup;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
